299a377ba7f783eafb9e9e37658e22c9721990c3
nwtks <44502906+nwtks@users.noreply.github.com>
2018-10-27 17:27:37 +0900

    add cjs.

--- dist/syncdom.js
+++ dist/syncdom.js
@@ -1,1 +1,165 @@
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.syncDom=t()}(this,function(){"use strict";var o=1;function c(e,t){if(e.nodeType===t.nodeType)if(e.nodeType===o){if(a=t,n=(i=e).getAttribute("data-domsame"),l=a.getAttribute("data-domsame"),n&&l&&n===l||a.isSameNode(i))return;if(function(e,t){var r={},i=e.firstChild;for(;i;){var a=i;i=i.nextSibling;var n=m(a);n&&(r[n]=a)}i=e.firstChild;var l=t.firstChild,o=0;for(;l;){o+=1;var d=l;l=l.nextSibling;var u=m(d),f=u?r[u]:null;if(f)delete r[u],f===i?i=i.nextSibling:e.insertBefore(f,i),c(f,d);else if(i){var s=i;i=i.nextSibling,h(r,s)?e.insertBefore(d,s):c(s,d)}else e.appendChild(d)}for(var v in r)e.removeChild(r[v]);var b=e.childNodes.length-o;for(;0<=--b;)e.removeChild(e.lastChild)}(e,t),e.nodeName===t.nodeName)!function(e,t){for(var r=e.attributes,i=r.length-1;0<=i;i-=1){var a=r[i],n=a.namespaceURI,l=a.localName;t.hasAttributeNS(n,l)||e.removeAttributeNS(n,l)}for(var o=t.attributes,d=o.length-1;0<=d;d-=1){var u=o[d],f=u.namespaceURI,s=u.localName,v=t.getAttributeNS(f,s),b=e.getAttributeNS(f,s);v!==b&&e.setAttributeNS(f,s,v)}var c=e.nodeName;if("INPUT"===c){p(e,t,"checked"),p(e,t,"disabled"),p(e,t,"readOnly");var m=t.value;e.value!==m&&(e.value=m),t.hasAttributeNS(null,"value")||e.removeAttribute("value")}else if("TEXTAREA"===c){p(e,t,"disabled"),p(e,t,"readOnly");var h=t.value;e.value!==h&&(e.value=h)}else"OPTION"===c?(p(e,t,"selected"),p(e,t,"disabled")):"SELECT"===c&&p(e,t,"disabled")}(e,t);else{for(var r=t.cloneNode();e.firstChild;)r.appendChild(e.firstChild);e.parentNode.replaceChild(r,e)}}else e.nodeValue!==t.nodeValue&&(e.nodeValue=t.nodeValue);else e.parentNode.replaceChild(t,e);var i,a,n,l}function p(e,t,r){e[r]!==t[r]&&(e[r]=t[r],e[r]?e.setAttribute(r.toLowerCase(),""):e.removeAttribute(r.toLowerCase()))}function m(e){if(e.nodeType===o)return e.getAttribute("data-domkey")}function h(e,t){for(var r in e)if(e[r]===t)return!0;return!1}return c});
+'use strict';
+
+var NODE_ELEMENT = 1;
+
+function syncDom(oldNode, newNode) {
+  if (oldNode.nodeType === newNode.nodeType) {
+    if (oldNode.nodeType === NODE_ELEMENT) {
+      if (isSameNode(oldNode, newNode)) {
+        return;
+      }
+      syncChildren(oldNode, newNode);
+      if (oldNode.nodeName === newNode.nodeName) {
+        syncAttrs(oldNode, newNode);
+      } else {
+        var newPrev = newNode.cloneNode();
+        while (oldNode.firstChild) {
+          newPrev.appendChild(oldNode.firstChild);
+        }
+        oldNode.parentNode.replaceChild(newPrev, oldNode);
+      }
+    } else {
+      if (oldNode.nodeValue !== newNode.nodeValue) {
+        oldNode.nodeValue = newNode.nodeValue;
+      }
+    }
+  } else {
+    oldNode.parentNode.replaceChild(newNode, oldNode);
+  }
+}
+
+function syncChildren(oldParent, newParent) {
+  var keyedNodes = {};
+  var oldNext = oldParent.firstChild;
+  while (oldNext) {
+    var oldNode = oldNext;
+    oldNext = oldNext.nextSibling;
+    var key = getKey(oldNode);
+    if (key) {
+      keyedNodes[key] = oldNode;
+    }
+  }
+
+  oldNext = oldParent.firstChild;
+  var newNext = newParent.firstChild;
+  var newCount = 0;
+  while (newNext) {
+    newCount += 1;
+    var newNode = newNext;
+    newNext = newNext.nextSibling;
+    var key$1 = getKey(newNode);
+    var keyedOld = key$1 ? keyedNodes[key$1] : null;
+    if (keyedOld) {
+      delete keyedNodes[key$1];
+      if (keyedOld === oldNext) {
+        oldNext = oldNext.nextSibling;
+      } else {
+        oldParent.insertBefore(keyedOld, oldNext);
+      }
+      syncDom(keyedOld, newNode);
+    } else if (oldNext) {
+      var oldNode$1 = oldNext;
+      oldNext = oldNext.nextSibling;
+      if (containsValue(keyedNodes, oldNode$1)) {
+        oldParent.insertBefore(newNode, oldNode$1);
+      } else {
+        syncDom(oldNode$1, newNode);
+      }
+    } else {
+      oldParent.appendChild(newNode);
+    }
+  }
+
+  for (var key$2 in keyedNodes) {
+    oldParent.removeChild(keyedNodes[key$2]);
+  }
+
+  var overCount = oldParent.childNodes.length - newCount;
+  while (--overCount >= 0) {
+    oldParent.removeChild(oldParent.lastChild);
+  }
+}
+
+function syncAttrs(oldNode, newNode) {
+  var oldAttrs = oldNode.attributes;
+  for (var i = oldAttrs.length - 1; i >= 0; i -= 1) {
+    var a = oldAttrs[i];
+    var ns = a.namespaceURI;
+    var n = a.localName;
+    if (!newNode.hasAttributeNS(ns, n)) {
+      oldNode.removeAttributeNS(ns, n);
+    }
+  }
+
+  var newAttrs = newNode.attributes;
+  for (var i$1 = newAttrs.length - 1; i$1 >= 0; i$1 -= 1) {
+    var a$1 = newAttrs[i$1];
+    var ns$1 = a$1.namespaceURI;
+    var n$1 = a$1.localName;
+    var v1 = newNode.getAttributeNS(ns$1, n$1);
+    var v2 = oldNode.getAttributeNS(ns$1, n$1);
+    if (v1 !== v2) {
+      oldNode.setAttributeNS(ns$1, n$1, v1);
+    }
+  }
+
+  var name = oldNode.nodeName;
+  if (name === "INPUT") {
+    syncBoolProp(oldNode, newNode, "checked");
+    syncBoolProp(oldNode, newNode, "disabled");
+    syncBoolProp(oldNode, newNode, "readOnly");
+    var value = newNode.value;
+    if (oldNode.value !== value) {
+      oldNode.value = value;
+    }
+    if (!newNode.hasAttributeNS(null, "value")) {
+      oldNode.removeAttribute("value");
+    }
+  } else if (name === "TEXTAREA") {
+    syncBoolProp(oldNode, newNode, "disabled");
+    syncBoolProp(oldNode, newNode, "readOnly");
+    var value$1 = newNode.value;
+    if (oldNode.value !== value$1) {
+      oldNode.value = value$1;
+    }
+  } else if (name === "OPTION") {
+    syncBoolProp(oldNode, newNode, "selected");
+    syncBoolProp(oldNode, newNode, "disabled");
+  } else if (name === "SELECT") {
+    syncBoolProp(oldNode, newNode, "disabled");
+  }
+}
+
+function syncBoolProp(oldNode, newNode, name) {
+  if (oldNode[name] !== newNode[name]) {
+    oldNode[name] = newNode[name];
+    if (oldNode[name]) {
+      oldNode.setAttribute(name.toLowerCase(), "");
+    } else {
+      oldNode.removeAttribute(name.toLowerCase());
+    }
+  }
+}
+
+function isSameNode(n1, n2) {
+  var eq1 = n1.getAttribute("data-domsame");
+  var eq2 = n2.getAttribute("data-domsame");
+  return (eq1 && eq2 && eq1 === eq2) || n2.isSameNode(n1);
+}
+
+function getKey(n) {
+  if (n.nodeType === NODE_ELEMENT) {
+    return n.getAttribute("data-domkey");
+  }
+}
+
+function containsValue(obj, v) {
+  for (var k in obj) {
+    if (obj[k] === v) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = syncDom;
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,22 @@
 import buble from "rollup-plugin-buble";
 import { uglify } from "rollup-plugin-uglify";
 
-export default {
-  input: "src/syncdom.js",
-  output: {
-    name: "syncDom",
-    file: "dist/syncdom.js",
-    format: "umd"
+export default [
+  {
+    input: "src/syncdom.js",
+    output: {
+      file: "dist/syncdom.js",
+      format: "cjs"
+    },
+    plugins: [buble()]
   },
-  plugins: [buble(), uglify()]
-};
+  {
+    input: "src/syncdom.js",
+    output: {
+      name: "syncDom",
+      file: "dist/syncdom-umd.js",
+      format: "umd"
+    },
+    plugins: [buble(), uglify()]
+  }
+];
