066b966dceec30f385f8e8ca80e227fe4f97345f
nwtks <44502906+nwtks@users.noreply.github.com>
2018-10-27 21:27:34 +0900

    code style.

--- dist/syncdom.js
+++ dist/syncdom.js
@@ -6,7 +6,7 @@ function syncDom(oldNode, newNode) {
   if (oldNode.nodeType === newNode.nodeType) {
     if (oldNode.nodeType === NODE_ELEMENT) {
       if (isSameNode(oldNode, newNode)) {
-        return;
+        return
       }
       syncChildren(oldNode, newNode);
       if (oldNode.nodeName === newNode.nodeName) {
@@ -104,29 +104,29 @@ function syncAttrs(oldNode, newNode) {
   }
 
   var name = oldNode.nodeName;
-  if (name === "INPUT") {
-    syncBoolProp(oldNode, newNode, "checked");
-    syncBoolProp(oldNode, newNode, "disabled");
-    syncBoolProp(oldNode, newNode, "readOnly");
+  if (name === 'INPUT') {
+    syncBoolProp(oldNode, newNode, 'checked');
+    syncBoolProp(oldNode, newNode, 'disabled');
+    syncBoolProp(oldNode, newNode, 'readOnly');
     var value = newNode.value;
     if (oldNode.value !== value) {
       oldNode.value = value;
     }
-    if (!newNode.hasAttributeNS(null, "value")) {
-      oldNode.removeAttribute("value");
+    if (!newNode.hasAttributeNS(null, 'value')) {
+      oldNode.removeAttribute('value');
     }
-  } else if (name === "TEXTAREA") {
-    syncBoolProp(oldNode, newNode, "disabled");
-    syncBoolProp(oldNode, newNode, "readOnly");
+  } else if (name === 'TEXTAREA') {
+    syncBoolProp(oldNode, newNode, 'disabled');
+    syncBoolProp(oldNode, newNode, 'readOnly');
     var value$1 = newNode.value;
     if (oldNode.value !== value$1) {
       oldNode.value = value$1;
     }
-  } else if (name === "OPTION") {
-    syncBoolProp(oldNode, newNode, "selected");
-    syncBoolProp(oldNode, newNode, "disabled");
-  } else if (name === "SELECT") {
-    syncBoolProp(oldNode, newNode, "disabled");
+  } else if (name === 'OPTION') {
+    syncBoolProp(oldNode, newNode, 'selected');
+    syncBoolProp(oldNode, newNode, 'disabled');
+  } else if (name === 'SELECT') {
+    syncBoolProp(oldNode, newNode, 'disabled');
   }
 }
 
@@ -134,7 +134,7 @@ function syncBoolProp(oldNode, newNode, name) {
   if (oldNode[name] !== newNode[name]) {
     oldNode[name] = newNode[name];
     if (oldNode[name]) {
-      oldNode.setAttribute(name.toLowerCase(), "");
+      oldNode.setAttribute(name.toLowerCase(), '');
     } else {
       oldNode.removeAttribute(name.toLowerCase());
     }
@@ -142,24 +142,24 @@ function syncBoolProp(oldNode, newNode, name) {
 }
 
 function isSameNode(n1, n2) {
-  var eq1 = n1.getAttribute("data-domsame");
-  var eq2 = n2.getAttribute("data-domsame");
-  return (eq1 && eq2 && eq1 === eq2) || n2.isSameNode(n1);
+  var eq1 = n1.getAttribute('data-domsame');
+  var eq2 = n2.getAttribute('data-domsame');
+  return (eq1 && eq2 && eq1 === eq2) || n2.isSameNode(n1)
 }
 
 function getKey(n) {
   if (n.nodeType === NODE_ELEMENT) {
-    return n.getAttribute("data-domkey");
+    return n.getAttribute('data-domkey')
   }
 }
 
 function containsValue(obj, v) {
   for (var k in obj) {
     if (obj[k] === v) {
-      return true;
+      return true
     }
   }
-  return false;
+  return false
 }
 
 module.exports = syncDom;
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
-import buble from "rollup-plugin-buble";
-import { uglify } from "rollup-plugin-uglify";
+import buble from 'rollup-plugin-buble'
+import { uglify } from 'rollup-plugin-uglify'
 
 export default [
   {
-    input: "src/syncdom.js",
+    input: 'src/syncdom.js',
     output: {
-      file: "dist/syncdom.js",
-      format: "cjs"
+      file: 'dist/syncdom.js',
+      format: 'cjs'
     },
     plugins: [buble()]
   },
   {
-    input: "src/syncdom.js",
+    input: 'src/syncdom.js',
     output: {
-      name: "syncDom",
-      file: "dist/syncdom-umd.js",
-      format: "umd"
+      name: 'syncDom',
+      file: 'dist/syncdom-umd.js',
+      format: 'umd'
     },
     plugins: [buble(), uglify()]
   }
-];
+]
